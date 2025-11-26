b8bd6fbb16dfee7a423aa1817e8693ee8154b319
Mica <maf.micaela@gmail.com>
2025-11-26 17:44:05 -0300

    Fix

--- gestion_alumnos/settings.py
+++ gestion_alumnos/settings.py
@@ -131,11 +131,11 @@ LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'usuarios:login'
 
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp-relay.brevo.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "9c9dbc001@smtp-brevo.com" 
-EMAIL_HOST_PASSWORD = ""
-DEFAULT_FROM_EMAIL = "maf.micaela@gmail.com"
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER") 
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
